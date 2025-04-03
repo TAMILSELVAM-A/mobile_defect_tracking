0163881c090b37748a727ba19f7e4a5ed2c13927
TAMILSELVAM-A <tamilselvamcr72001@gmail.com>
2025-04-03 16:30:04 +0530

    Commmit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage"; // Fixed semicolon typo
 import DefectTrackingTable from "./components/DefectTable";
-import { Login } from "@mui/icons-material";
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-
   return (
     <Router>
       <Routes>
